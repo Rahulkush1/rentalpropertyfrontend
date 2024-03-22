a804bb380b125c1f9bb9ea336aaac2e93772381a
rails <rahul9713428971@gmail.com>
2024-03-22 17:47:38 +0530

    added DataTable

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Home from "./component/Home/Home";
 import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import SignUp from "./component/Authentication/Signup";
 import { createBrowserRouter } from "react-router-dom";
-import "./App.css";
+
 import Login from "./component/Authentication/Login";
 import store from "./store";
-import {  clearErrors, loadUser, setCredentials, setUserError } from "./Slice/userSlice";
+import {
+  clearErrors,
+  loadUser,
+  setCredentials,
+  setUserError,
+} from "./Slice/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Property from "./component/Property/Property";
 import PropertyDetails from "./component/Property/PropertyDetails";
@@ -21,45 +26,76 @@ import UserDial from "./component/Helper/UserDial";
 import { setProperty } from "./Slice/propertySlice";
 import { useGetPropertiesQuery } from "./services/auth/propertyService";
 import Appointment from "./component/Appointment/Appointment";
+import "./App.css";
+import Booking from "./component/Booking/Booking";
+import UserProfile from "./component/User/UserProfile";
+import ProtectedRoute from "./component/Route/ProtectedRoute";
+import About from "./component/About/About";
+import Contact from "./component/Contact/Contact";
 
 export default function App() {
-  const dispatch = useDispatch()
-  const { userInfo, error,  isAuthenticated, loading } = useSelector(
+  const dispatch = useDispatch();
+  const { userInfo, error, isAuthenticated, loading } = useSelector(
     (state) => state.user
   );
-  const { data,  isFetching } = useGetUserDetailsQuery("userDetails", {
+  const { data, isFetching } = useGetUserDetailsQuery("userDetails", {
     pollingInterval: 9000000,
   });
   const { data1, isloading } = useGetPropertiesQuery("properties", {
     pollingInterval: 9000000,
   });
- 
+
   useEffect(() => {
     if (data) {
       dispatch(setCredentials(data));
     }
-    if(data1){
-        dispatch(setProperty(data1))
+    if (data1) {
+      dispatch(setProperty(data1));
     }
   }, [dispatch, data, data1]);
 
   return (
-
-    <>
+    <div className="app">
       <BrowserRouter>
-        <Navbar />
-        {
-          isAuthenticated && <UserDial user={userInfo}  />
-        }
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/properties" element={<Property />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/properties/:id" element={<PropertyDetails />} />
-          <Route path="/users/appointments" element={<Appointment />} />
-        </Routes>
+        <div className="content">
+          <Navbar />
+          {isAuthenticated && <UserDial user={userInfo} />}
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/properties" element={<Property />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/properties/:id" element={<PropertyDetails />} />
+            <Route path="/about" element= {<About />} />
+            <Route path="/contact" element= {<Contact />} />
+
+            <Route
+              path="/users/appointments"
+              element={
+                <ProtectedRoute>
+                  <Appointment />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/users/bookings"
+              element={
+                <ProtectedRoute>
+                  <Booking />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/user/profile"
+              element={
+                <ProtectedRoute>
+                  <UserProfile />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
         <Footer />
         <ToastContainer
           position="top-right"
@@ -74,7 +110,7 @@ export default function App() {
           theme="colored"
         />
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 export const BASE_URL = "http://localhost:5000/api/v1/";
