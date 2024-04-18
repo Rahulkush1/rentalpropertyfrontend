f86825bb513052bfc471c2cf25d01b0888790c6f
rails <rahul9713428971@gmail.com>
2024-04-18 19:13:19 +0530

    modified css to make responsive

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,41 +1,63 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Link } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { useSelector } from "react-redux";
+import { CoPresentSharp } from "@mui/icons-material";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="rgba(241, 241, 241, 0.868)">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
-
 }
 
 const defaultTheme = createTheme({
-    typography: {
-        body1: {
-          color: 'white'
-        },
-
-      }
+  typography: {
+    body1: {
+      color: "white",
+    },
+  },
 });
 
+const getRandomCities = (cities, numCities) => {
+  const shuffledCities = [...cities];
+
+  for (let i = shuffledCities.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledCities[i], shuffledCities[j]] = [
+      shuffledCities[j],
+      shuffledCities[i],
+    ];
+  }
+
+  return shuffledCities.slice(0, numCities);
+};
+
 export default function Footer() {
+  const { cities, state_cities } = useSelector((state) => state.cities);
+  let randomCities = getRandomCities(state_cities, 5);
+  console.log(randomCities);
   return (
     <ThemeProvider theme={defaultTheme}>
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh", // Ensure footer sticks to the bottom
         }}
       >
         <CssBaseline />
@@ -45,19 +67,89 @@ export default function Footer() {
           sx={{
             py: 3,
             px: 2,
-            mt: 'auto',
+            mt: "auto",
             backgroundColor: (theme) =>
-              theme.palette.mode === 'dark'
+              theme.palette.mode === "dark"
                 ? theme.palette.grey[200]
                 : theme.palette.grey[800],
           }}
         >
-          <Container maxWidth="sm">
-            <Typography variant="body1">
-              My sticky footer can be found here.
-            </Typography>
-            <Copyright />
+          <Container className="d-flex justify-content-evenly">
+            {/* Cities Section */}
+            <div className="section-1">
+              <Typography variant="h6" gutterBottom>
+                Cities
+              </Typography>
+              <div className="grey">
+                {randomCities &&
+                  randomCities.map((city) => (
+                    <Link className="text-decoration-none cities-name">
+                      <Typography
+                        variant="body1"
+                        className="grey  "
+                        key={city.id}
+
+                      >
+                        {city.name}
+                      </Typography>
+                    </Link>
+                  ))}
+              </div>
+            </div>
+
+            {/* Policy and Conditions Section */}
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                Policy and Conditions
+              </Typography>
+              <div className="text-center">
+                <Typography variant="body1" className="grey">
+                  Terms & Conditions
+                </Typography>
+                <Typography variant="body1" className="grey">
+                  Privacy Policy
+                </Typography>
+                <Typography variant="body1" className="grey">
+                  About
+                </Typography>
+                <Typography variant="body1" className="grey">
+                  Contact
+                </Typography>
+              </div>
+            </Box>
+
+            {/* Social Profile Section */}
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                Social Profiles
+              </Typography>
+              <IconButton
+                color="inherit"
+                aria-label="Facebook"
+                href="https://www.facebook.com/example"
+                target="_blank"
+              >
+                <FacebookIcon />
+              </IconButton>
+              <IconButton
+                color="inherit"
+                aria-label="Twitter"
+                href="https://twitter.com/example"
+                target="_blank"
+              >
+                <TwitterIcon />
+              </IconButton>
+              <IconButton
+                color="inherit"
+                aria-label="Instagram"
+                href="https://www.instagram.com/example"
+                target="_blank"
+              >
+                <InstagramIcon />
+              </IconButton>
+            </Box>
           </Container>
+          <Copyright />
         </Box>
       </Box>
     </ThemeProvider>
--- src/component/Helper/Card.js
+++ src/component/Helper/Card.js
@@ -1,19 +1,28 @@
-
 import React from "react";
-import Card from "@mui/material/Card";
+// import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import Formatprice from "./FormatPrice";
-import './Card.css'
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card } from "antd";
+import "./Card.css";
+const { Meta } = Card;
 
 export default function PropCard({ data }) {
   return (
-    <Link to={`/properties/${data.attributes.id}`} className="text-decoration-none" >
-    <Card className="image-card"  >
+    <Link
+      to={`/properties/${data.attributes.id}`}
+      className="text-decoration-none"
+    >
+      {/* <Card className="image-card"  >
       <CardActionArea>
         <CardMedia
           component="img"
@@ -46,8 +55,27 @@ export default function PropCard({ data }) {
           </Button>
         </CardContent>
       </CardActionArea>
-    </Card>
+    </Card> */}
+
+      <div class="card property-card" >
+        <img
+          src={`http://localhost:5000/${data.attributes.avatar[0]}`}
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body property-content">
+          <h5 class="card-title"> {data.attributes.name}</h5>
+          <p class="card-text grey">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content Lizards are a widespread.
+          </p>
+          <div className="d-flex  justify-content-around">
+            <Button variant="contained"> Show </Button>
+
+            <Button variant="outlined"> Schedule </Button>
+          </div>
+        </div>
+      </div>
     </Link>
   );
 }
-
